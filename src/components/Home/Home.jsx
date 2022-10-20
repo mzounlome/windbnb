78e1e74b11cfcab825e2ccdda7a6f4beb89dd58a
Medrick Zounlome <mzounlome@gmail.com>
2022-10-20 08:17:47 -0400

    MDP-12

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { HomeHeader } from "./Home-Styles";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <HomeHeader>windbnb</HomeHeader>
+    </div>
+  );
+};
+
+export default Home;
