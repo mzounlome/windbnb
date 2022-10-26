3d309af1500e0da47b8999551192b132a648bdc9
Medrick Zounlome <mzounlome@gmail.com>
2022-10-26 08:35:20 -0400

    Co-authored-by: Romaric Zounlome <rzounlom@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import "./App.scss";
 
 import Gallery from "./components/Gallery/Gallery";
 import NavBar from "./components/Nav/NavBar";
+import { locations } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [currentLocation, setCurrentLocation] = useState("Finland");
+  const handleLocation = () => {
+    switch (currentLocation) {
+      case "Italy":
+        return locations.Italy;
+      case "France":
+        return locations.France;
+      case "Sweden":
+        return locations.Sweden;
+      default:
+        return locations.Finland;
+    }
+  };
+
+  const stays = handleLocation();
+
   return (
     <div className="App">
       <NavBar />
       <div>
-        <Gallery />
+        <Gallery stays={stays} />
       </div>
     </div>
   );
--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "../Gallery/images/logo.png";
+import logo from "../../assets/img/logo.png";
 
 function NavBar() {
   return (
